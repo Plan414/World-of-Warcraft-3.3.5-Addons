b086d5a4573aca639245aa5903875127c27ac986
Plan414 <62799885+Plan414@users.noreply.github.com>
2021-10-03 07:42:00 -0400

    Add files via upload

---
+++ OneBank3/localization/deDE.lua
@@ -0,0 +1,24 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("OneBank3", "deDE")
+
+if L then 
+    --@non-debug@
+    L["Bank Bag"] = "Banktasche"
+L["Fifth Bank Bag"] = "Fünfte Banktasche"
+L["First Bank Bag"] = "Erste Banktasche"
+L["Fourth Bank Bag"] = "Vierte Banktasche"
+L["Second Bank Bag"] = "Zweite Banktasche"
+L["Seventh Bank Bag"] = "Siebte Banktasche"
+L["Sixth Bank Bag"] = "Sechste Banktasche"
+L["Specific Bag Filters"] = "Spezielle Taschenfilter"
+L["%s's Bank Bags"] = "%s's Banktaschen"
+L["Third Bank Bag"] = "Dritte Banktasche"
+L["Toggles the display of your Fifth Bank Bag."] = "Schaltet das Anzeigen der fünften Banktasche an/aus"
+L["Toggles the display of your First Bank Bag."] = "Schaltet das Anzeigen der ersten Banktasche an/aus"
+L["Toggles the display of your Fourth Bank Bag."] = "Schaltet das Anzeigen der vierten Banktasche an/aus"
+L["Toggles the display of your Second Bank Bag."] = "Schaltet das Anzeigen der zweiten Banktasche an/aus"
+L["Toggles the display of your Seventh Bank Bag."] = "Schaltet das Anzeigen der siebten Banktasche an/aus"
+L["Toggles the display of your Sixth Bank Bag."] = "Schaltet das Anzeigen der sechsten Banktasche an/aus"
+L["Toggles the display of your Third Bank Bag."] = "Schaltet das Anzeigen der dritten Banktasche an/aus"
+
+    --@end-non-debug@
+end
---
+++ OneBank3/localization/esES.lua
@@ -0,0 +1,24 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("OneBank3", "esES")
+
+if L then 
+    --@non-debug@
+    L["Bank Bag"] = "Bolsa Inicial del Banco"
+L["Fifth Bank Bag"] = "Quinta Bolsa del Banco"
+L["First Bank Bag"] = "Primera Bolsa del Banco"
+L["Fourth Bank Bag"] = "Cuarta Bolsa del Banco"
+L["Second Bank Bag"] = "Segunda Bolsa del Banco"
+L["Seventh Bank Bag"] = "Séptima Bolsa del Banco"
+L["Sixth Bank Bag"] = "Sexta Bolsa del Banco"
+L["Specific Bag Filters"] = "Filtrado de bolsas específicas"
+L["%s's Bank Bags"] = "%s's Bolsas del Banco"
+L["Third Bank Bag"] = "Tercera Bolsa del Banco"
+L["Toggles the display of your Fifth Bank Bag."] = "Muestra/Oculta la Quinta Bolsa del Banco"
+L["Toggles the display of your First Bank Bag."] = "Muestra/Oculta la Primera Bolsa del Banco"
+L["Toggles the display of your Fourth Bank Bag."] = "Muestra/Oculta la Cuarta Bolsa del Banco"
+L["Toggles the display of your Second Bank Bag."] = "Muestra/Oculta la Segunda Bolsa del Banco"
+L["Toggles the display of your Seventh Bank Bag."] = "Muestra/Oculta la Séptima Bolsa del Banco"
+L["Toggles the display of your Sixth Bank Bag."] = "Muestra/Oculta la Sexta Bolsa del Banco"
+L["Toggles the display of your Third Bank Bag."] = "Muestra/Oculta la Tercera Bolsa del Banco"
+
+    --@end-non-debug@
+end
